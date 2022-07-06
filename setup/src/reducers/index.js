b77cea9991bd6f6591244f0a26906452f2d191c8
NghiaDangTran <33323750+NghiaDangTran@users.noreply.github.com>
2022-07-05 19:26:21 -0700

    Delete setup directory

--- setup/src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import productReducer from './product'
-import {combineReducers} from 'redux'
-const rootReducer = combineReducers({
-    product : productReducer
-});
-export default rootReducer
-
--- setup/src/store.js
+++
@@ -1,5 +0,0 @@
-import rootReducer from './reducers/index'
-import {createStore, applyMiddleware} from 'redux'
-import thunk from 'redux-thunk'
-const store = createStore(rootReducer, applyMiddleware(thunk))
-export default store
